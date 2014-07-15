14accf6a65456d916c9b6011a158e9cb5508ff39
sonson <yyoshida@d-itlab.co.jp>
2014-07-15 15:50:40 +0900

    Added ROMO SDK. Test to send message when ROMO is connected/disconnected.

--- RMBTReceiver/RMBTReceiverSample/ViewController.m
+++ RMBTReceiver/RMBTReceiverSample/ViewController.m
@@ -9,12 +9,18 @@
 #import "ViewController.h"
 #import "RMBTDeviceSelectViewController.h"
 #import "RMBTReceiver.h"
+#import <RMCore/RMCore.h>
 
-@interface ViewController ()
-
+@interface ViewController () <RMCoreDelegate>
+@property (nonatomic, strong) RMCoreRobot<HeadTiltProtocol, DriveProtocol, LEDProtocol> *robot;
 @end
 
 @implementation ViewController
+
+- (void)viewDidLoad {
+	[super viewDidLoad];
+	[RMCore setDelegate:self];
+}
 
 - (IBAction)open:(id)sender {
 	[self presentViewController:[RMBTDeviceSelectViewController viewController] animated:YES completion:nil];
@@ -22,6 +28,21 @@
 
 - (IBAction)send:(id)sender {
 	[[RMBTReceiver sharedInstance] sendString:@"test, this is test"];
+}
+
+- (void)robotDidConnect:(RMCoreRobot *)robot {
+    // Currently the only kind of robot is Romo3, so this is just future-proofing
+    if (robot.isDrivable && robot.isHeadTiltable && robot.isLEDEquipped) {
+        self.robot = (RMCoreRobot<HeadTiltProtocol, DriveProtocol, LEDProtocol> *) robot;
+		[[RMBTReceiver sharedInstance] sendString:@"ROMO is connected."];
+    }
+}
+
+- (void)robotDidDisconnect:(RMCoreRobot *)robot {
+    if (robot == self.robot) {
+        self.robot = nil;
+		[[RMBTReceiver sharedInstance] sendString:@"ROMO is disconnected."];
+    }
 }
 
 @end
